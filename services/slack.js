97df0c499d765dae0334740282447b2d130d86c8
Marco Zaccheroni <m.zaccheroni@cosmobile.com>
2018-11-28 23:53:04 +0100

    Yo

--- services/slack.js
+++ services/slack.js
@@ -1,69 +1,93 @@
 const Service = require('../common/Service')
+const fs = require('fs').promises
 
 module.exports = class SlackService extends Service {
-  async process (payload) {
-    if (payload[payload] !== undefined) {
-      // Answer to interactive message
-      let action = JSON.parse(payload.payload).actions[0]
-      switch (action.name) {
-        case 'send':
-          let pepe = JSON.parse(action.value)
-          return {
-            'response_type': 'in_channel',
-            'as_user': true,
-            'attachments': [
-              {
-                'title': pepe.mood,
-                'image_url': pepe.image
-              }
-            ],
-            'delete_original': true
-          }
-        default:
-          return {
-            'delete_original': true
-          }
-      }
-    } else {
-      // New message
-      return {
-        as_user: true,
-        'attachments': [
-          {
-            'title': payload.text,
-            'image_url': 'http://i.imgur.com/dytv5Xh.png',
-            'callback_id': 'pepe',
-            'actions': [
-              {
-                'name': 'send',
-                'text': 'Send Pepe',
-                'style': 'primary',
-                'type': 'button',
-                'value': JSON.stringify({
-                  'mood': payload.text,
-                  'image': 'http://i.imgur.com/dytv5Xh.png'
-                })
-              }, /*
-                      {
-                          "name": "change",
-                          "text": "Change Pepe",
-                          "type": "button",
-                          "value": JSON.stringify({
-                            "mood": request.payload.text,
-                            "image": "http://i.imgur.com/dytv5Xh.png"
-                          })
-                      }, */
-              {
-                'name': 'cancel',
-                'text': 'Discard Pepe',
-                'style': 'danger',
-                'type': 'button',
-                'value': 'cancel'
-              }
-            ]
-          }
-        ]
-      }
+
+    async process(payload) {
+        if (payload[payload] !== undefined) {
+            // Answer to interactive message
+            let action = JSON.parse(payload.payload).actions[0]
+            let pepe
+            switch (action.name) {
+                case 'send':
+                    pepe = JSON.parse(action.value)
+                    return {
+                        response_type: 'in_channel',
+                        as_user: true,
+                        attachments: [
+                            {
+                                title: pepe.mood,
+                                image_url: pepe.image
+                            }
+                        ],
+                        delete_original: true
+                    }
+                case 'change':
+                    pepe = JSON.parse(action.value)
+                    let new_image = pepe.image
+                    while (pepe.image !== new_image) {
+                        new_image = this.random()
+                    }
+                    return this.interactive(payload.text, new_image, true)
+                default:
+                    return {
+                        delete_original: true
+                    }
+            }
+        } else {
+            // New message
+            return this.interactive(payload.text, this.random())
+        }
     }
-  }
+
+    interactive(text, image_url, override = false) {
+        let msg = {
+            as_user: true,
+            attachments: [
+                {
+                    title: text,
+                    image_url: image_url,
+                    callback_id: 'pepe',
+                    actions: [
+                        {
+                            name: 'send',
+                            text: 'Send Pepe',
+                            style: 'primary',
+                            type: 'button',
+                            value: JSON.stringify({
+                                mood: text,
+                                image: image_url
+                            })
+                        },
+                        {
+                            name: 'change',
+                            text: 'Another Pepe',
+                            type: 'button',
+                            value: JSON.stringify({
+                                mood: text,
+                                image: image_url
+                            })
+                        },
+                        {
+                            name: 'cancel',
+                            text: 'Discard Pepe',
+                            style: 'danger',
+                            type: 'button',
+                            value: 'cancel'
+                        }
+                    ]
+                }
+            ]
+        }
+        if (override === true) {
+            msg.delete_original = true
+        }
+        return msg
+    }
+
+    async random() {
+
+        return 'http://i.imgur.com/dytv5Xh.png'
+
+    }
 }
