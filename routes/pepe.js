241647475c6299b9576a7a1f4e0b3d5a2d5dec3d
Marco Zaccheroni <m.zaccheroni@cosmobile.com>
2018-12-05 09:35:02 +0100

    async uuuuuuuurrrrph

--- routes/pepe.js
+++ routes/pepe.js
@@ -1,14 +1,13 @@
 const sh = require('../common/servicesHandler')
 const fs = require('fs').promises
-let lastAction = {}
-let lastError
+
 module.exports = function register (server, options) {
   require('inert')
 
   server.route({
     method: 'GET',
     path: '/{mood}',
-    handler: function (request, h) {
+    handler: async function (request, h) {
       console.log(request.params.mood)
       const path = __dirname + '/../assets/pepes'
       console.log(path)
