699e94fc51eda05e91d32a8e20ed22aaf4ca7a37
Marco Zaccheroni <m.zaccheroni@cosmobile.com>
2018-12-05 09:56:40 +0100

    fix pepe route

--- routes/pepe.js
+++ routes/pepe.js
@@ -2,12 +2,12 @@ const sh = require('../common/servicesHandler')
 const fs = require('fs').promises
 
 module.exports = function register (server, options) {
-  require('inert')
 
   server.route({
     method: 'GET',
     path: '/{mood}',
     handler: async function (request, h) {
+      await server.register(require('inert'));
       console.log(request.params.mood)
       const path = __dirname + '/../assets/pepes'
       console.log(path)
